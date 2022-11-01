720611741f497cce5041163f0dfa26ffe3e0fdda
PujaNeralwar <102819825+PujaNeralwar@users.noreply.github.com>
2022-11-01 14:38:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -13,3 +13,42 @@ const App = () => {
 
 
 export default App;
+
+
+                    // let oldGrid = blueBoard;
+                    // let newArray = cloneDeep(blueBoard);
+
+                    // console.table(newArray);
+                    // for(let i=0;i<GridSize;i++){
+                    //     let b = newArray[i];
+                    //     let slow = 0;
+                    //     let fast = 1;
+                    //    while(slow<GridSize){
+                    //         if(fast===GridSize){
+                    //             fast = slow+1;
+                    //             slow++;
+                    //             continue;
+                    //         }
+                    //         if(b[slow]===0 && b[fast]===0){
+                    //             fast++;
+                    //         }else if(b[slow]===0 && b[fast]!==0){
+                    //             b[slow]=b[fast];
+                    //             b[fast] = 0;
+                    //             fast++;
+                    //         }else if(b[slow]!==0 && b[fast]===0){
+                    //             fast++;
+                    //         }else if(b[slow]!==0 && b[fast]!==0){
+                    //             if(b[slow]===b[fast]){
+                    //                 b[slow] = b[slow] + b[fast];
+                    //                 b[fast] = 0;
+                    //                 fast = slow+1;
+                    //                 slow++;
+                    //             }else{
+                    //                 slow++;
+                    //                 fast = slow+1;
+                    //             }
+                    //         }
+
+
+                    //     }
+                    // }
